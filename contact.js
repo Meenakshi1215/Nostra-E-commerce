bcd334ff8b7480a867c0e64d5e28962e748665ff
Meenakshi1215 <91046694+Meenakshi1215@users.noreply.github.com>
2024-04-04 14:28:57 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,3 @@
+var name = document.getElementById("name");
+var email = document.getElementById("email");
+var message = document.getElementById();
---
+++ index.js
@@ -0,0 +1,10 @@
+var sidenav = document.querySelector(".side-navbar");
+
+
+function showNavbar(){
+    sidenav.style.left = "0"
+}
+
+function closeNavbar(){
+    sidenav.style.left = "-60%"
+}
