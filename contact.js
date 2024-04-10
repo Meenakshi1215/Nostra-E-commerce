01aba3b6fa95d2c2f1e6ec22a17f61c75e6e89e0
Meenakshi1215 <91046694+Meenakshi1215@users.noreply.github.com>
2024-04-10 11:14:02 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,3 +1,13 @@
+var sidenav = document.querySelector(".side-navbar");
+function showNavbar(){
+    sidenav.style.left = "0"
+}
+
+function closeNavbar(){
+    sidenav.style.left = "-60%"
+}
+
+
 var name = document.getElementById("name");
 var email = document.getElementById("email");
-var message = document.getElementById();
+var message = document.getElementById("message");
